bec555cbb730c60f3fd592c17a3da7a1eac2f842
Marten Veldthuis <marten@veldthuis.com>
2011-10-26 10:05:45 +0200

    Add and clean up documentation

--- lib/mongoid-app_settings.rb
+++ lib/mongoid-app_settings.rb
@@ -4,14 +4,35 @@ module Mongoid
   module AppSettings
     extend ActiveSupport::Concern
 
-    class Record
+    class Record #:nodoc:
       include Mongoid::Document
       identity type: String
       store_in :settings
     end
 
     module ClassMethods
-      def method_missing(name, *args, &block)
+      # Defines a setting. Options can include:
+      #
+      # * default -- Specify a default value
+      #
+      # Example usage:
+      #
+      #   class MySettings
+      #     include Mongoid::AppSettings
+      #     setting :organization_name, :default => "demo"
+      #   end
+      def setting(name, options = {})
+        settings[name.to_s] = options
+      end
+      
+      # Force a reload from the database
+      def reload
+        @record = nil
+      end
+
+      protected
+
+      def method_missing(name, *args, &block) # :nodoc:
         if name.to_s =~ /^(.*)=$/ and setting_defined?($1)
           self[$1] = args[0]
         elsif setting_defined?(name.to_s)
@@ -21,24 +42,20 @@ module Mongoid
         end
       end
 
-      def settings
+      def settings # :nodoc:
         @settings ||= {}
       end
 
-      def setting_defined?(name)
+      def setting_defined?(name) # :nodoc:
         settings.include?(name)
       end
 
-      def setting(name, options = {})
-        settings[name.to_s] = options
-      end
-      
-      def record
+      def record # :nodoc:
         return @record if @record
         @record = Record.find_or_create_by(id: "settings")
       end
 
-      def [](name)
+      def [](name) # :nodoc:
         if record.attributes.include?(name)
           record.read_attribute(name)
         else
@@ -46,14 +63,9 @@ module Mongoid
         end
       end
 
-      def []=(name, value)
+      def []=(name, value) # :nodoc:
         record.set(name, value)
       end
-
-      def reload
-        @record = nil
-      end
     end
-
   end
 end

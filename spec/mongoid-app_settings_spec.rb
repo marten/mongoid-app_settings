9b5912ea42fa4911d3c6363893e1b2c50b67f694
Marten Veldthuis <marten@veldthuis.com>
2012-01-02 11:17:59 +0100

    Reorg specs into groups

--- spec/mongoid-app_settings_spec.rb
+++ spec/mongoid-app_settings_spec.rb
@@ -13,81 +13,107 @@ describe "Mongoid::AppSettings" do
     other_settings
   end
 
-  it "should be possible to save a setting" do 
-    settings.instance_eval { setting :something }
-    settings.something = "some nice value"
-    settings.something.should == "some nice value"
+  describe "defining settings" do
+    it "should be possible to define a setting" do
+      settings.instance_eval { setting :something }
+      settings.something.should be_nil
+    end
+
+    it "should be possible to specify default values" do
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo.should == "bar"
+    end
+
+    it "should be possible to use defaults of other settings in defaults" do
+      settings.instance_eval { setting :foo, :default => "bar"
+                              setting :baz, :default => "#{foo} quux" }
+      settings.baz.should == "bar quux"
+    end
+
+    it "should be possible to use values of other settings in defaults" do
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo = "baz"
+      settings.instance_eval { setting :qux, :default => "#{foo} quux" }
+      settings.qux.should === "baz quux"
+    end
+
+    it "should define fields on the record" do
+      settings.instance_eval { setting :something }
+      expect { 
+        settings.send(:record).something 
+      }.not_to raise_error(NoMethodError)
+    end
   end
 
-  it "should define fields on the record" do
-    settings.instance_eval { setting :something }
-    expect { 
-      settings.send(:record).something 
-    }.not_to raise_error(NoMethodError)
-  end
+  describe "setting values" do
+    it "should be possible to save a setting" do 
+      settings.instance_eval { setting :something }
+      settings.something = "some nice value"
+      settings.something.should == "some nice value"
+    end
 
-  it "should save settings outside of itself" do
-    settings.instance_eval { setting :something }
-    other_settings.instance_eval { setting :something }
+    it "should save settings to mongodb" do
+      settings.instance_eval { setting :something }
+      other_settings.instance_eval { setting :something }
 
-    settings.something = "some nice value"
-    other_settings.something.should == "some nice value"
-  end
+      settings.something = "some nice value"
+      other_settings.something.should == "some nice value"
+    end
 
-  it "should be possible to specify default values" do
-    settings.instance_eval { setting :foo, :default => "bar" }
-    settings.foo.should == "bar"
-  end
+    it "should be possible to overwrite a default value" do
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo = "baz"
+      settings.foo.should == "baz"
+    end
 
-  it "should be possible to use defaults of other settings in defaults" do
-    settings.instance_eval { setting :foo, :default => "bar"
-                             setting :baz, :default => "#{foo} quux" }
-    settings.baz.should == "bar quux"
-  end
+    it "should be possible to overwrite a value with something else" do
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo = "baz"
+      settings.foo = "quux"
+      settings.foo.should == "quux"
+    end
 
-  it "should be possible to use values of other settings in defaults" do
-    settings.instance_eval { setting :foo, :default => "bar" }
-    settings.foo = "baz"
-    settings.instance_eval { setting :qux, :default => "#{foo} quux" }
-    settings.qux.should === "baz quux"
-  end
+    it "should be possible to unset a value, reverting to default" do
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo = "baz"
+      settings.delete(:foo)
+      settings.foo.should == "bar"
+    end
 
-  it "should be possible to overwrite a default value" do
-    settings.instance_eval { setting :foo, :default => "bar" }
-    settings.foo = "baz"
-    settings.foo.should == "baz"
-  end
-
+    it "should be possible to overwrite a value with false" do
+      # At one point, Mongoid didn't support Record#set with nil/false
+      # This spec is here to prevent regression
   it "should be possible to overwrite a value with something else" do
     settings.instance_eval { setting :foo, :default => "bar" }
-    settings.foo = "baz"
-    settings.foo = "quux"
-    settings.foo.should == "quux"
+
+    it "should be possible to overwrite a value with false" do
+      # At one point, Mongoid didn't support Record#set with nil/false
+      # This spec is here to prevent regression
+      settings.instance_eval { setting :foo, :default => "bar" }
+      settings.foo = "baz"
+      settings.foo = nil
+      settings.foo.should == nil
+    end
   end
 
-  it "should be possible to overwrite a value with something falsey" do
-    settings.instance_eval { setting :foo, :default => "bar" }
-    settings.foo = "baz"
-    settings.foo = false
-    settings.foo.should == false
-  end
+  describe "reload behaviour" do
+    it "should not reload for every call" do
+      settings.instance_eval { setting :foo }
+      other_settings.instance_eval { setting :foo }
+      other_settings.foo # force other_settings to load the record
 
-  it "should not constantly reload" do
-    settings.instance_eval { setting :foo }
-    other_settings.instance_eval { setting :foo }
-    other_settings.foo # force other_settings to load the record
+      settings.foo = "bar"
+      other_settings.foo.should be_nil
+    end
 
-    settings.foo = "bar"
-    other_settings.foo.should be_nil
-  end
+    it "should be possible to reload" do
+      settings.instance_eval { setting :foo }
+      other_settings.instance_eval { setting :foo }
+      other_settings.foo # force other_settings to load the record
 
-  it "should be possible to reload" do
-    settings.instance_eval { setting :foo }
-    other_settings.instance_eval { setting :foo }
-    other_settings.foo # force other_settings to load the record
-
-    settings.foo = "bar"
-    other_settings.reload
-    other_settings.foo.should == "bar"
+      settings.foo = "bar"
+      other_settings.reload
+      other_settings.foo.should == "bar"
+    end
   end
 end
